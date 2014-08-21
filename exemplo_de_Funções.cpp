1f21da1cff7d1adf7c66bafb95f8c8919976ddb9
Genival Veloso de França Neto <netoovf@live.com>
2014-08-21 09:30:47 -0300

    Create exemplo_de_Funções.cpp

---
+++ exemplo_de_Funções.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int adicao (int a, int b) // criou uma função "adicao", que engloba duas variáveis  (a) e (b) ambas do tipo "int" 
+{
+int r;
+r=a+b;
+return r;
+}
+  int main() {
+  int z;
+  z = adicao (9,7)
+  cout << "seu resultado é: \n" << z;
+  }
