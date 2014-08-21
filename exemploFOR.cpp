9a2773ca47f8d48fb7bc30aefe0739ef38e1476d
Genival Veloso de França Neto <netoovf@live.com>
2014-08-21 08:46:45 -0300

    Create exemploFOR.cpp

---
+++ exemploFOR.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+  for (int n=0;n<100 ;n++){ // for irá ter uma variável N = 0, repetirá o loop até que atenda a "n<100,
+    cout << n << " , ";     //  encrementando de 1 em 1.
+  }
+  
+  cout << "Fim do programa \n";  
+    
+  }
